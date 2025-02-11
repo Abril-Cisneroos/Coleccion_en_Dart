0364a0b6bcff30fd38e9c1cc589e5e497e150698
Abril-Cisneroos <a.22308051281162@cbtis128.edu.mx>
2025-02-11 16:53:56 +0000

    hola lista

---
+++ lista1.dart
@@ -0,0 +1,13 @@
+void main() {
+  //LISTA TIPO ENTERO 3 ELEMENTOS
+  List<int> numeros = [10, 20, 30];
+  print("Lista de numero enteros:");
+  print(numeros);
+  print("primer elemento: ${numeros[0]}");
+  print("segundo elemento: ${numeros[1]}");
+  print("tercer elemento: ${numeros[2]}");
+  print("Elementos de la lista usando ciclo for: ");
+  for (int i = 0; i < numeros.length; i++) {
+    print(numeros[i]);
+  }
+}
