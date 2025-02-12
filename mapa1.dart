0a559eed0a384415db6a54f10e8f3419d5d5e477
Abril-Cisneroos <a.22308051281162@cbtis128.edu.mx>
2025-02-12 20:02:44 +0000

    intento

---
+++ mapa1.dart
@@ -0,0 +1,21 @@
+
+void maind(){
+    print("Frida Abril Cisneros Hernandez 22308051281162 gpo 6 j");
+    Map<int, String> alumnos = {
+      1: "Frida", 
+      2: "Abril", 
+      3: "juan", 
+      4: "luis"};
+      print("mapa de alumnos:");
+      print(alumnos);
+
+      print("iterar un map con forEach");
+      alumnos.forEach((key, value) {
+        print("$key,  $value");
+      });
+
+      print("iterar un map con for in");
+      for (var key in alumnos.keys) {
+        print("$key, ${alumnos[key]}");
+      }
+}
